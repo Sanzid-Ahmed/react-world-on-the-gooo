72ef21c3e21c89fa305127936bb363052e338b72
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-09-19 19:22:19 +0600

    added thirs commit

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,18 +1,34 @@
 import React, { use } from 'react';
+import { useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
+    const [visitedCountries, setVisitedCountries] = useState([])
+
+
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
     const contriesData = use(countriesPromise);
     const countries = contriesData.countries
     console.log(countries);
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country 
-                    key ={country.cca3.cca3}
-                    country={country}></Country>)
-            }
+            <h3>Total country visited: {visitedCountries.length}</h3>
+            
+            <div className='Countries'>
+                {
+                    countries.map(country => <Country
+                        key={country.cca3.cca3}
+                        country={country}
+                        handleVisitedCountries = {handleVisitedCountries}
+                        ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useState } from "react";
+import './Country.css'
 
-const Country = ({country}) => {
+
+const Country = ({country, handleVisitedCountries}) => {
+
+    // eslint-disable-next-line no-undef
+    const [visited, setVisited] = useState(false);
+
     // console.log(country.flags.flags);
+
+    const handleVisited = () =>{
+        if(visited){
+            // eslint-disable-next-line no-undef
+            setVisited(false);
+        }
+        else{
+            setVisited(true);
+        }
+
+        handleVisitedCountries(country)
+    }
+
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'} `}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 30000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>{visited?"Visited":"Not Visited"}</button>
         </div>
     );
 };
